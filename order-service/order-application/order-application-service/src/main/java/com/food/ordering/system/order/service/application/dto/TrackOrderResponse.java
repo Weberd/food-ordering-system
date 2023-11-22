387d1ccd77d03b46a14a612b55ddd177d2a72c7d
Aleksei Udodov <weberdster@gmail.com>
2023-11-22 17:19:10 +0400

    added OrderStatus enum

--- order-service/order-application/order-application-service/src/main/java/com/food/ordering/system/order/service/application/dto/TrackOrderResponse.java
+++ order-service/order-application/order-application-service/src/main/java/com/food/ordering/system/order/service/application/dto/TrackOrderResponse.java
@@ -1,6 +1,8 @@
 package com.food.ordering.system.order.service.application.dto;
 
+import com.food.ordering.system.domain.value.OrderStatus;
+
 import java.util.UUID;
 
-public record TrackOrderResponse(UUID trackingId, String status) {
+public record TrackOrderResponse(UUID orderId, OrderStatus status) {
 }
