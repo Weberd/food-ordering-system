59accac4b9972d575735fa46cc75e23eda8ca9e1
Aleksei Udodov <weberdster@gmail.com>
2023-12-06 16:52:16 +0500

    added query for findAllBefore

--- order-service/order-application/order-application-service/src/main/java/com/food/ordering/system/order/service/application/port/output/OrderCreationOutboxRepository.java
+++ order-service/order-application/order-application-service/src/main/java/com/food/ordering/system/order/service/application/port/output/OrderCreationOutboxRepository.java
@@ -3,11 +3,13 @@ package com.food.ordering.system.order.service.application.port.output;
 import com.food.ordering.system.domain.value.OrderId;
 import com.food.ordering.system.order.service.application.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.Instant;
 
 public interface OrderCreationOutboxRepository extends JpaRepository<Order, OrderId> {
     Order save(Order order);
     void deleteById(OrderId id);
+    @Query("select o from Order o where s.created < :time")
     Iterable<Order> findAllBefore(Instant time);
 }
