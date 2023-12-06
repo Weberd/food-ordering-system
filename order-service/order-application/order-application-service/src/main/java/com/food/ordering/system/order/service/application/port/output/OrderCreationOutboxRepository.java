bdb17cb65477c7129e43f3c7706062cc632f5300
Aleksei Udodov <weberdster@gmail.com>
2023-12-06 16:42:38 +0500

    jpa interfaces with auto implementations

--- order-service/order-application/order-application-service/src/main/java/com/food/ordering/system/order/service/application/port/output/OrderCreationOutboxRepository.java
+++ order-service/order-application/order-application-service/src/main/java/com/food/ordering/system/order/service/application/port/output/OrderCreationOutboxRepository.java
@@ -2,11 +2,12 @@ package com.food.ordering.system.order.service.application.port.output;
 
 import com.food.ordering.system.domain.value.OrderId;
 import com.food.ordering.system.order.service.application.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.Instant;
 
-public interface OrderCreationOutboxRepository {
-    void save(Order order);
+public interface OrderCreationOutboxRepository extends JpaRepository<Order, OrderId> {
+    Order save(Order order);
     void deleteById(OrderId id);
     Iterable<Order> findAllBefore(Instant time);
 }
--- order-service/order-application/order-application-service/src/main/java/com/food/ordering/system/order/service/application/port/output/OrderRepository.java
+++ order-service/order-application/order-application-service/src/main/java/com/food/ordering/system/order/service/application/port/output/OrderRepository.java
@@ -2,10 +2,11 @@ package com.food.ordering.system.order.service.application.port.output;
 
 import com.food.ordering.system.domain.value.OrderId;
 import com.food.ordering.system.order.service.application.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface OrderRepository {
-    void save(Order order);
+public interface OrderRepository extends JpaRepository<Order, OrderId> {
     Optional<Order> findById(OrderId id);
+    Order save(Order order);
 }
