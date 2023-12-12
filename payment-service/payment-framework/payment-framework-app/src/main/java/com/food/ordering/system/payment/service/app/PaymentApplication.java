cff88d5c7be23e7a48e91a6097af2d040d3798f0
Aleksei Udodov <weberdster@gmail.com>
2023-12-13 01:42:25 +0500

    added payment application

--- payment-service/payment-framework/payment-framework-app/src/main/java/com/food/ordering/system/payment/service/app/PaymentApplication.java
+++ payment-service/payment-framework/payment-framework-app/src/main/java/com/food/ordering/system/payment/service/app/PaymentApplication.java
@@ -1,4 +1,4 @@
-package com.food.ordering.system.order.service.app;
+package com.food.ordering.system.payment.service.app;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -11,9 +11,9 @@ import org.springframework.kafka.core.KafkaAdmin;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.food.ordering.system")
-public class OrderApplication {
+public class PaymentApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OrderApplication.class, args);
+        SpringApplication.run(PaymentApplication.class, args);
     }
 
     @Value("${spring.kafka.topic.payment-request-topic}")
@@ -21,12 +21,6 @@ public class OrderApplication {
 
     @Value("${spring.kafka.topic.payment-response-topic}")
     private String paymentResponseTopic;
-
-    @Value("${spring.kafka.topic.restaurant-request-topic}")
-    private String restaurantRequestTopic;
-
-    @Value("${spring.kafka.topic.restaurant-response-topic}")
-    private String restaurantResponseTopic;
 
     @Bean
     public KafkaAdmin.NewTopics topics() {
@@ -38,31 +32,15 @@ public class OrderApplication {
             TopicBuilder.name(paymentResponseTopic)
                     .partitions(2)
                     .replicas(2)
-                    .build(),
-            TopicBuilder.name(restaurantRequestTopic)
-                    .partitions(2)
-                    .replicas(2)
-                    .build(),
-            TopicBuilder.name(restaurantResponseTopic)
-                    .partitions(2)
-                    .replicas(2)
                     .build()
         );
     }
 
     @KafkaListener(id = "id", topics = {
-            "${spring.kafka.topic.payment-response-topic}",
+            "${spring.kafka.topic.payment-request-topic}",
         }
     )
-    public void listenPaymentResponse(String in) {
-        System.out.println(in);
-    }
-
-    @KafkaListener(id = "id", topics = {
-            "${spring.kafka.topic.restaurant-response-topic}",
-        }
-    )
-    public void listenRestaurantResponse(String in) {
+    public void listenPaymentRequest(String in) {
         System.out.println(in);
     }
 }
