e7b272d6afb08351d98d14e2e755045fb192a9f5
Aleksei Udodov <weberdster@gmail.com>
2023-12-13 01:42:37 +0500

    added restaurant application

---
+++ restaurant-service/restaurant-framework/restaurant-framework-app/src/main/java/com/food/ordering/system/restaurant/service/app/RestaurantApplication.java
@@ -0,0 +1,46 @@
+package com.food.ordering.system.restaurant.service.app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.food.ordering.system")
+public class RestaurantApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantApplication.class, args);
+    }
+
+    @Value("${spring.kafka.topic.restaurant-request-topic}")
+    private String restaurantRequestTopic;
+
+    @Value("${spring.kafka.topic.restaurant-response-topic}")
+    private String restaurantResponseTopic;
+
+    @Bean
+    public KafkaAdmin.NewTopics topics() {
+        return new KafkaAdmin.NewTopics(
+            TopicBuilder.name(restaurantRequestTopic)
+                    .partitions(2)
+                    .replicas(2)
+                    .build(),
+            TopicBuilder.name(restaurantResponseTopic)
+                    .partitions(2)
+                    .replicas(2)
+                    .build()
+        );
+    }
+
+    @KafkaListener(id = "id", topics = {
+            "${spring.kafka.topic.restaurant-request-topic}",
+        }
+    )
+    public void listenRestaurantRequest(String in) {
+        System.out.println(in);
+    }
+}
