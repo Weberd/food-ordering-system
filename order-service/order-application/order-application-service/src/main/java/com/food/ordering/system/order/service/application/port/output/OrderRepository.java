cef5d00dabed58d5cf29d179fbaae583891498aa
Aleksei Udodov <weberdster@gmail.com>
2023-12-22 17:45:41 +0500

    added save method

--- order-service/order-application/order-application-service/src/main/java/com/food/ordering/system/order/service/application/port/output/OrderRepository.java
+++ order-service/order-application/order-application-service/src/main/java/com/food/ordering/system/order/service/application/port/output/OrderRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface OrderRepository extends JpaRepository<Order, OrderId> {
+public interface OrderRepository {
     Optional<Order> findById(OrderId id);
+
+    void save(Order order);
 }
