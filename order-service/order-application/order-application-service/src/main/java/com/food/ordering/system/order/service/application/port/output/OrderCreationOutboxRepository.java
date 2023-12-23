161443c2dd733d63f3c904c41af2bdba51741496
Aleksei Udodov <weberdster@gmail.com>
2023-12-23 21:13:45 +0500

    removed data features from application level repo

--- order-service/order-application/order-application-service/src/main/java/com/food/ordering/system/order/service/application/port/output/OrderCreationOutboxRepository.java
+++ order-service/order-application/order-application-service/src/main/java/com/food/ordering/system/order/service/application/port/output/OrderCreationOutboxRepository.java
@@ -3,12 +3,12 @@ package com.food.ordering.system.order.service.application.port.output;
 import com.food.ordering.system.domain.value.OrderId;
 import com.food.ordering.system.order.service.application.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.time.Instant;
 
-public interface OrderCreationOutboxRepository extends JpaRepository<Order, OrderId> {
+public interface OrderCreationOutboxRepository {
     void deleteById(OrderId id);
-    @Query("select o from Order o where s.created < :time")
     Iterable<Order> findAllBefore(Instant time);
+
+    void save(Order order);
 }
