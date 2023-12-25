5d454823feea3b1cc920b60866a8359389aa1989
Aleksei Udodov <weberdster@gmail.com>
2023-12-25 21:51:15 +0500

    added required args constructor

--- order-service/order-data/src/main/java/com/food/ordering/system/order/data/mapper/OrderMapperImpl.java
+++ order-service/order-data/src/main/java/com/food/ordering/system/order/data/mapper/OrderMapperImpl.java
@@ -3,10 +3,12 @@ package com.food.ordering.system.order.data.mapper;
 import com.food.ordering.system.domain.value.OrderId;
 import com.food.ordering.system.order.data.entity.OrderEntity;
 import com.food.ordering.system.order.service.application.entity.Order;
+import lombok.RequiredArgsConstructor;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public final class OrderMapperImpl implements OrderMapper {
     public Order toOrder(OrderEntity orderEntity) {
         return new Order(
