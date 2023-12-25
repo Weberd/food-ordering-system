fa1566e66d70bc4bcfbe5a3b64697668c5a1b782
Aleksei Udodov <weberdster@gmail.com>
2023-12-25 21:51:25 +0500

    removed import

--- order-service/order-application/order-application-service/src/main/java/com/food/ordering/system/order/service/application/port/output/OrderRepository.java
+++ order-service/order-application/order-application-service/src/main/java/com/food/ordering/system/order/service/application/port/output/OrderRepository.java
@@ -2,7 +2,6 @@ package com.food.ordering.system.order.service.application.port.output;
 
 import com.food.ordering.system.domain.value.OrderId;
 import com.food.ordering.system.order.service.application.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
