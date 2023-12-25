38e8e68bbc2cd16e89ce0cee0bfaa7c3e04e1f93
Aleksei Udodov <weberdster@gmail.com>
2023-12-25 21:51:57 +0500

    replaced constructor with required args constructor

--- order-service/order-data/src/main/java/com/food/ordering/system/order/data/repo/OrderRepositoryImpl.java
+++ order-service/order-data/src/main/java/com/food/ordering/system/order/data/repo/OrderRepositoryImpl.java
@@ -4,20 +4,16 @@ import com.food.ordering.system.domain.value.OrderId;
 import com.food.ordering.system.order.data.mapper.OrderMapper;
 import com.food.ordering.system.order.service.application.entity.Order;
 import com.food.ordering.system.order.service.application.port.output.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Component
+@RequiredArgsConstructor
 public class OrderRepositoryImpl implements OrderRepository {
     private final OrderJpaRepository orderJpaRepository;
     private final OrderMapper orderMapper;
-
-    public OrderRepositoryImpl(
-            OrderJpaRepository orderJpaRepository,
-            OrderMapper orderMapper
-    ) {
-        this.orderJpaRepository = orderJpaRepository;
-        this.orderMapper = orderMapper;
-    }
 
     @Override
     public Optional<Order> findById(OrderId id) {
