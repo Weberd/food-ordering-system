4cbd4b09264a913d0039d192cbbcc5b86ca7758e
Aleksei Udodov <weberdster@gmail.com>
2023-12-31 00:50:22 +0500

    added validation to create order command class

--- order-service/order-application/order-application-service/src/main/java/com/food/ordering/system/order/service/application/dto/CreateOrderCommand.java
+++ order-service/order-application/order-application-service/src/main/java/com/food/ordering/system/order/service/application/dto/CreateOrderCommand.java
@@ -2,14 +2,13 @@ package com.food.ordering.system.order.service.application.dto;
 
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@RequiredArgsConstructor
 public class CreateOrderCommand {
-    @Size(max = 255)
-    private final String description;
-
-    public CreateOrderCommand(String description) {
-        this.description = description;
-    }
-
+    @Size(max = 5)
+    private String description;
 }
