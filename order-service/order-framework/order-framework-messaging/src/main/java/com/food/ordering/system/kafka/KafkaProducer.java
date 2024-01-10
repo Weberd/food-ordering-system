3e03a37e2a2468541c3642266af237a908707141
Aleksei Udodov <weberdster@gmail.com>
2024-01-11 00:51:35 +0500

    moved kafka producer

--- order-service/order-framework/order-framework-messaging/src/main/java/com/food/ordering/system/kafka/KafkaProducer.java
+++ order-service/order-framework/order-framework-messaging/src/main/java/com/food/ordering/system/kafka/KafkaProducer.java
@@ -1,4 +1,4 @@
-package com.food.ordering.system.kafka.producer;
+package com.food.ordering.system.kafka;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
