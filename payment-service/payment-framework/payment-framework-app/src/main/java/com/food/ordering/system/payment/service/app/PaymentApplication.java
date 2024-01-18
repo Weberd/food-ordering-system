7110343fe950c189086a5556dc760a9c2a470648
Aleksei Udodov <weberdster@gmail.com>
2024-01-18 13:09:15 +0500

    unused import

--- payment-service/payment-framework/payment-framework-app/src/main/java/com/food/ordering/system/payment/service/app/PaymentApplication.java
+++ payment-service/payment-framework/payment-framework-app/src/main/java/com/food/ordering/system/payment/service/app/PaymentApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaAdmin;
 
