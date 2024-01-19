45e745086b7928783ea34e68ba2dd4b9f30034d3
Aleksei Udodov <weberdster@gmail.com>
2024-01-19 18:17:00 +0500

    fixed restaurant kafka consumer

--- restaurant-service/restaurant-framework/restaurant-framework-messaging/src/main/java/com/food/ordering/system/restaurant/service/kafka/consumer/OrderKafkaConsumer.java
+++ restaurant-service/restaurant-framework/restaurant-framework-messaging/src/main/java/com/food/ordering/system/restaurant/service/kafka/consumer/OrderKafkaConsumer.java
@@ -7,28 +7,28 @@ import com.food.ordering.system.common.messaging.event.InventoryReservedEvent;
 import com.food.ordering.system.common.messaging.producer.KafkaProducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-@KafkaListener(id = "${spring.kafka.topic.restaurant-request-topic}", topics = {
-    "${spring.kafka.topic.restaurant-request-topic}",
-})
 public class OrderKafkaConsumer {
     @Value("${spring.kafka.topic.restaurant-response-topic}")
-    private final String restaurantResponseTopic;
+    private String restaurantResponseTopic;
 
     private final KafkaProducer<InventoryReservedEvent> inventoryReservedEventProducer;
     private final KafkaProducer<InventoryReservationCanceledEvent> inventoryReservationCanceledEventProducer;
 
-    @KafkaHandler
+    @KafkaListener(id = "inventory-reserved", topics = {
+        "${spring.kafka.topic.restaurant-request-topic}"
+    })
     public void inventoryReserved(ReserveInventoryCommand command) {
         inventoryReservedEventProducer.sendMessage(restaurantResponseTopic, new InventoryReservedEvent(command.orderId()));
     }
 
-    @KafkaHandler
+    @KafkaListener(id = "cancel-inventory-reservation", topics = {
+        "${spring.kafka.topic.restaurant-request-topic}"
+    })
     public void cancelInventoryReservation(CancelInventoryReservationCommand command) {
         inventoryReservationCanceledEventProducer.sendMessage(restaurantResponseTopic, new InventoryReservationCanceledEvent(command.orderId()));
     }
